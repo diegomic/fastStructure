874b039073d4f76cf0b3f9d25368d27c93e84ddc
Shaurita Hutchins <shaurita.d.hutchins@gmail.com>
2019-12-21 10:59:42 -0600

    Added extensions in better format.

--- setup.py
+++ setup.py
@@ -5,32 +5,20 @@ from Cython.Distutils import build_ext
 import numpy
 import sys
 
-# setup bed parser
-ext_modules = [Extension("parse_bed", ["parse_bed.pyx"])]
-
-setup(
-    name = 'parse_bed',
-    cmdclass = {'build_ext': build_ext},
-    include_dirs=[numpy.get_include(), '.'],
-    ext_modules = ext_modules
-)
-
-# setup structure format parser
-ext_modules = [Extension("parse_str", ["parse_str.pyx"])]
-
-setup(
-    name = 'parse_str',
-    cmdclass = {'build_ext': build_ext},
-    include_dirs=[numpy.get_include(), '.'],
-    ext_modules = ext_modules
-)
-
-# setup fastStructure
-ext_modules = [Extension("fastStructure", ["fastStructure.pyx"])]
-
-setup(
-    name = 'fastStructure',
-    cmdclass = {'build_ext': build_ext},
-    include_dirs=[numpy.get_include(), '.', 'vars/'],
-    ext_modules = ext_modules
-)
+setup(name='fastStructure',
+      install_requires=[
+          'logzero',
+          'numpy',
+          'scipy',
+      ],
+      description="fastStructure is a fast algorithm for inferring population structure from large SNP genotype data.",
+      license="MIT",
+      cmdclass={'build_ext': build_ext},
+      # see https://stackoverflow.com/questions/21826137/cython-setup-py-for-several-pyx
+      ext_modules=[Extension("parse_bed", ["parse_bed.pyx"],
+                             include_dirs=[numpy.get_include(), '.']),
+                   Extension("parse_str", ["parse_str.pyx"],
+                             include_dirs=[numpy.get_include(), '.']),
+                   Extension("fastStructure", ["fastStructure.pyx"],
+                             include_dirs=[numpy.get_include(), '.', 'vars/'])]
+      )
