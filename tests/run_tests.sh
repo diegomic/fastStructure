0f098329f2ce6072403f76c3fc33455b98e2ff86
Shaurita Hutchins <shaurita.d.hutchins@gmail.com>
2019-12-21 12:06:22 -0600

    Fixed tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python structure.py -K 3 --input=data/testdata --output=testoutput_simple --full --seed=100
+python ../structure.py -K 3 --input=data/testdata --output=testoutput_simple --full --seed=100
 
-python structure.py -K 3 --input=data/testdata --output=testoutput_logistic --full --seed=100 --prior=logistic
+python ../structure.py -K 3 --input=data/testdata --output=testoutput_logistic --full --seed=100 --prior=logistic
